6cefad562a6e9f2c6437fcde63f907e17036934c
marcioaleson <marcioaleson@gmail.com>
2016-10-26 00:45:56 -0300

    Add comments in blog pages.

--- rubyblog_6/config/routes.rb
+++ rubyblog_6/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :posts
+  #resources :posts
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
     root "posts#index"
@@ -25,6 +25,9 @@ Rails.application.routes.draw do
   #       get 'sold'
   #     end
   #   end
+  resources :posts do 
+       resources :comments
+  end
 
   # Example resource route with sub-resources:
   #   resources :products do

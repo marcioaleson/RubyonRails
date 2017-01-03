a80f0072e45ce9d7cb977823a6863be9613c28e4
marcioaleson <marcioaleson@gmail.com>
2017-01-03 20:56:06 -0300

    Post edit controller and view.

--- muse/app/controllers/posts_controller.rb
+++ muse/app/controllers/posts_controller.rb
@@ -24,15 +24,20 @@ class PostsController < ApplicationController
     end
 
     def edit
-    	
+
     end
 
     def update
-    	
+        if @post.update(post_params)
+            redirect_to @post
+        else
+            render 'edit'            
+        end    	
     end
 
     def destroy
-    	
+    	@post.destroy
+        redirect_to root_path
     end
 
     private

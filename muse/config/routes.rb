0931f2a59d903bcfb2defdb285de24dcb7bd195c
marcioaleson <marcioaleson@gmail.com>
2017-02-08 00:52:39 -0300

    Added post like and dislike count.

--- muse/config/routes.rb
+++ muse/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resources :posts do 
+    resources :posts do
+        member do
+            get "like", to: "posts#upvote"
+            get "dislike", to: "posts#downvote"
+        end 
     	resources :comments
     end
 
